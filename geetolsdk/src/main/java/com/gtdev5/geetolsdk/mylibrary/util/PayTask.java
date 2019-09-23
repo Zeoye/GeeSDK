2fc59486f582649a50273bdd867992da111818e1
airry <649338320@qq.com>
2019-09-23 17:33:45 +0800

    重写PayTask以供外部使用

---
+++ geetolsdk/src/main/java/com/gtdev5/geetolsdk/mylibrary/util/PayTask.java
@@ -0,0 +1,13 @@
+package com.gtdev5.geetolsdk.mylibrary.util;
+
+import android.app.Activity;
+
+/**
+ * Created by ZL on 2019/9/23
+ */
+
+public class PayTask extends com.alipay.sdk.app.PayTask {
+    public PayTask(Activity activity) {
+        super(activity);
+    }
+}
