fd1658471137028810d907c5ed43647ea81d12ed
airry <649338320@qq.com>
2019-09-24 08:47:43 +0800

    自定义PayTask继承alipay的方法供外部使用

--- geetolsdk/src/main/java/com/gtdev5/geetolsdk/mylibrary/util/PayTask.java
+++ geetolsdk/src/main/java/com/gtdev5/geetolsdk/mylibrary/util/PayTask.java
@@ -2,6 +2,11 @@ package com.gtdev5.geetolsdk.mylibrary.util;
 
 import android.app.Activity;
 
+import com.alipay.sdk.app.H5PayCallback;
+import com.alipay.sdk.util.H5PayResultModel;
+
+import java.util.Map;
+
 /**
  * Created by ZL on 2019/9/23
  */
@@ -9,5 +14,50 @@ import android.app.Activity;
 public class PayTask extends com.alipay.sdk.app.PayTask {
     public PayTask(Activity activity) {
         super(activity);
+    }
+
+    @Override
+    public synchronized Map<String, String> payV2(String s, boolean b) {
+        return super.payV2(s, b);
+    }
+
+    @Override
+    public synchronized String pay(String s, boolean b) {
+        return super.pay(s, b);
+    }
+
+    @Override
+    public synchronized H5PayResultModel h5Pay(String s, boolean b) {
+        return super.h5Pay(s, b);
+    }
+
+    @Override
+    public synchronized String fetchOrderInfoFromH5PayUrl(String s) {
+        return super.fetchOrderInfoFromH5PayUrl(s);
+    }
+
+    @Override
+    public synchronized String fetchTradeToken() {
+        return super.fetchTradeToken();
+    }
+
+    @Override
+    public String getVersion() {
+        return super.getVersion();
+    }
+
+    @Override
+    public synchronized boolean payInterceptorWithUrl(String s, boolean b, H5PayCallback h5PayCallback) {
+        return super.payInterceptorWithUrl(s, b, h5PayCallback);
+    }
+
+    @Override
+    public void showLoading() {
+        super.showLoading();
+    }
+
+    @Override
+    public void dismissLoading() {
+        super.dismissLoading();
     }
 }
